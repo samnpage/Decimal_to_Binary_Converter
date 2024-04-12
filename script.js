14a6018cc79944d27fea85fb1a830afa85f6e0f9
Sam Page <samnpage96@gmail.com>
2024-04-12 15:32:59 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// In this project, you'll build a decimal and binary converter and learn about both number systems. You'll also learn about recursion by using it to perform the conversions.
+
+// All of the HTML and CSS for this project has been provided for you.
