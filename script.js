cdf7f8a743d1366960f51fa24d0324cc7c6b3473
Sam Page <samnpage96@gmail.com>
2024-04-12 17:20:01 -0400

    1. Created function to check if user has provided the proper input. 2. Setup event listeners for when the user clicks the convert button, or presses the 'enter' key when inserting an input. 3. Created function to convert a decimal number to binary.

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 // In this project, you'll build a decimal and binary converter and learn about both number systems. You'll also learn about recursion by using it to perform the conversions.
 
 // All of the HTML and CSS for this project has been provided for you.
+
+// Elements
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+    let binary = "";
+
+    if (input === 0) {
+        binary = "0";
+    }
+
+    while (input > 0) {
+        binary = (input % 2) + binary;
+        input = Math.floor(input / 2);
+    }
+
+    result.innerText = binary;
+};
+
+const checkUserInput = () => {
+    if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+        alert("Please provide a decimal number");
+        return 
+    }
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+numberInput.addEventListener("keydown", (e) => {
+    if(e.key === 'Enter') {
+        checkUserInput();
+    }
+});
